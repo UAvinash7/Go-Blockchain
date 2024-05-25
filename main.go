ba3dd1dda9ce60af5e925e0b24cc81b86aaf38a6
Avinash Upadhyayula <36662440+UAvinash7@users.noreply.github.com>
2024-05-25 19:19:49 +0530

    main.go
    main.go file contains the ability to add new blocks, mine blocks, and view the full blockchain

--- main.go
+++ main.go
@@ -1,1 +1,116 @@
 
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+// Block represents a block in the blockchain
+type Block struct {
+	Index     int
+	Timestamp string
+	Data      string
+	Hash      string
+	PrevHash  string
+}
+
+// Blockchain is a slice of blocks
+var Blockchain []Block
+
+func calculateHash(block Block) string {
+	record := string(block.Index) + block.Timestamp + block.Data + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
+}
+
+func generateBlock(oldBlock Block, data string) Block {
+	var newBlock Block
+
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = time.Now().String()
+	newBlock.Data = data
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = calculateHash(newBlock)
+
+	return newBlock
+}
+
+func isBlockValid(newBlock, oldBlock Block) bool {
+	if oldBlock.Index+1 != newBlock.Index {
+		return false
+	}
+
+	if oldBlock.Hash != newBlock.PrevHash {
+		return false
+	}
+
+	if calculateHash(newBlock) != newBlock.Hash {
+		return false
+	}
+
+	return true
+}
+
+func replaceChain(newBlocks []Block) {
+	if len(newBlocks) > len(Blockchain) {
+		Blockchain = newBlocks
+	}
+}
+
+func handleGetBlockchain(w http.ResponseWriter, r *http.Request) {
+	bytes, err := json.MarshalIndent(Blockchain, "", "  ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", string(bytes))
+}
+
+func handleWriteBlock(w http.ResponseWriter, r *http.Request) {
+	var m Block
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&m); err != nil {
+		respondWithJSON(w, r, http.StatusBadRequest, r.Body)
+		return
+	}
+	defer r.Body.Close()
+
+	if isBlockValid(m, Blockchain[len(Blockchain)-1]) {
+		newBlockchain := append(Blockchain, m)
+		replaceChain(newBlockchain)
+		log.Println("Block added")
+	}
+
+	respondWithJSON(w, r, http.StatusCreated, m)
+}
+
+func respondWithJSON(w http.ResponseWriter, r *http.Request, code int, payload interface{}) {
+	response, err := json.MarshalIndent(payload, "", "  ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("HTTP 500: Internal Server Error"))
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func main() {
+	genesisBlock := Block{0, time.Now().String(), "Genesis Block", "", ""}
+	genesisBlock.Hash = calculateHash(genesisBlock)
+	Blockchain = append(Blockchain, genesisBlock)
+
+	http.HandleFunc("/blockchain", handleGetBlockchain)
+	http.HandleFunc("/write", handleWriteBlock)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
