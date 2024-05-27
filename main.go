12f5f35e20b3a1cdfc86036c27c07b8e97853e5b
Avinash Upadhyayula <36662440+UAvinash7@users.noreply.github.com>
2024-05-27 18:53:50 +0530

    Update main.go
    Adding support for multiple server instances, where each server instance initializes its own local copy of the blockchain from the db.

--- main.go
+++ main.go
@@ -8,21 +8,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync"
 	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Block represents a block in the blockchain
 type Block struct {
-	Index     int
-	Timestamp string
-	Data      string
-	Hash      string
-	PrevHash  string
+	Index     int    `json:"index"`
+	Timestamp string `json:"timestamp"`
+	Data      string `json:"data"`
+	Hash      string `json:"hash"`
+	PrevHash  string `json:"prevHash"`
 }
 
 // Blockchain is a slice of blocks
@@ -30,6 +32,7 @@ var Blockchain []Block
 var mutex = &sync.Mutex{}
 var client *mongo.Client
 var ctx = context.TODO()
+var serverAddress string
 
 func calculateHash(block Block) string {
 	record := string(block.Index) + block.Timestamp + block.Data + block.PrevHash
@@ -78,7 +81,7 @@ func handleGetBlockchain(w http.ResponseWriter, r *http.Request) {
 	defer mutex.Unlock()
 
 	collection := client.Database("blockchain").Collection("blocks")
-	cur, err := collection.Find(ctx, nil)
+	cur, err := collection.Find(ctx, bson.D{})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -88,7 +91,8 @@ func handleGetBlockchain(w http.ResponseWriter, r *http.Request) {
 	var blocks []Block
 	for cur.Next(ctx) {
 		var block Block
-		if err := cur.Decode(&block); err != nil {
+		err := cur.Decode(&block)
+		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
@@ -146,16 +150,26 @@ func saveBlockchain(block Block) {
 }
 
 func initMongoDB() {
+	var err error
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, _ = mongo.Connect(ctx, clientOptions)
-	err := client.Ping(ctx, nil)
+	client, err = mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("Connected to MongoDB!")
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
+	var err error
+	serverAddress = os.Getenv("SERVER_ADDRESS")
+	if serverAddress == "" {
+		serverAddress = "localhost:8080"
+	}
+
 	initMongoDB()
 
 	r := mux.NewRouter()
@@ -164,5 +178,9 @@ func main() {
 
 	http.Handle("/", r)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Printf("Starting server at %s\n", serverAddress)
+	err = http.ListenAndServe(serverAddress, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
